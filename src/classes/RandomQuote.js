a705d59d3746d879628b7163ed6a99d72bad405f
Stanislav <alina.malina.ivanova.1989@gmail.com>
2025-01-21 18:28:04 +0200

    Get random quotes via public API service

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -8,6 +8,18 @@ class RandomQuote {
     const { id, text, author } = quotes[randomIndex];
     return new Quote(id, text, author);
   }
+
+  static getRandomQuoteViaAPI() {
+    const url = 'https://quoteslate.vercel.app/api/quotes/random';
+    const option = { headers: { 'Content-Type': 'application/json' } };
+
+    return fetch(url, option)
+      .then((response) => response.json())
+      .then(({ id, quote, author }) => new Quote(id, quote, author))
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  }
 }
 
 export default RandomQuote;
