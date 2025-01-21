0602783a66b57a2a97d8ed16becd7940c09cc668
Stanislav <alina.malina.ivanova.1989@gmail.com>
2025-01-21 12:18:31 +0200

    Initial version of the quotes app using OOP and classes - chosing random quote from the array jf quotes

---
+++ src/classes/RandomQuote.js
@@ -0,0 +1,13 @@
+import quotes from '../data/quotes.js';
+import MathUtils from '../Utils/MathUtils.js';
+import Quote from './Quote.js';
+
+class RandomQuote {
+  static getRandomQuote() {
+    const randomIndex = MathUtils.generateRandomInt(quotes.length);
+    const { id, text, author } = quotes[randomIndex];
+    return new Quote(id, text, author);
+  }
+}
+
+export default RandomQuote;
---
+++ src/data/quotes.js
@@ -0,0 +1,39 @@
+const quotes = [
+  {
+    id: 'c718475e',
+    text: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    id: '3452d8aa',
+    text: 'Life is 10% what happens to us and 90% how we react to it',
+    author: 'Charles R. Swindoll',
+  },
+  {
+    id: '56487b49',
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    id: '56487b49',
+    text: "Believe you can and you're halfway there",
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: 'ie3736f8',
+    text: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+  },
+  {
+    id: 'ea08d955',
+    text: "Don't watch the clock; do what it does. Keep going",
+    author: 'Sam Levenson',
+  },
+  {
+    id: '9ac6bee8',
+    text: 'Keep your face always toward the sunshine—and shadows will fall behind you',
+    author: 'Walt Whitman',
+  },
+];
+
+export default quotes;
